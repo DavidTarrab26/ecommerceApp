58c69159936e0bd599481bc2aad35f682c25b63f
davidtarrab <dudutarrab@gmail.com>
2022-10-21 02:37:56 -0300

    Login terminado

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,1 +1,4 @@
+export const API_KEY = "AIzaSyBkiWigvkS4Wpdof4Rq21sQ4ozB7NATA0g"
+export const URL_AUTH_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
+export const URL_AUTH_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 export const URL_API = 'https://ecommerceapp-7a8a6-default-rtdb.firebaseio.com/'
